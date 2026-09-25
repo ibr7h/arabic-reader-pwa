33141811377a3e2d0323bc3b2702ce54503d188e
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-25 19:17:44 +0300

    feat: add Hurufi letter positions PWA (sw.js)

---
+++ letter-positions-pwa/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "hurufi-letter-positions-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "../assets/icons/icon-192.png",
+  "../assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE && key.startsWith("hurufi-letter-positions-")).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
