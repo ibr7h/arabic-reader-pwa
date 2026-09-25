3d15b88daca11c11574bf43bdb5bb158db544fc9
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-25 22:53:00 +0300

    Bump Hurufi service worker for v1.7.1

--- letter-positions-pwa/sw.js
+++ letter-positions-pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "hurufi-letter-positions-v16";
-const APP_VERSION = "1.7.0";
+const CACHE = "hurufi-letter-positions-v17";
+const APP_VERSION = "1.7.1";
 const SHELL = [
   ["./index.html", "./index.html?v="+APP_VERSION],
   ["./styles.css", "./styles.css?v="+APP_VERSION],
